fe9278e90fcb5e6e4ffadc2fa2807cbb6d80b929
Ewir <ewir1@bitbucket.org>
2018-12-21 15:44:19 -0400

    nuevo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { PreciosComponent } from './components/precios/precios.component';
+import { ProtegidaComponent } from './components/protegida/protegida.component';
+import { AuthGuardService } from './services/auth-guard.service';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'precios', component: PreciosComponent },
+  { path: 'protegida', component: ProtegidaComponent, canActivate: [AuthGuardService] },
+  { path: '**', pathMatch: 'full', redirectTo: 'home'}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { useHash: true})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
